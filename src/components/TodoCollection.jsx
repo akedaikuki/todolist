dede5e2c71ed761b4ed980d76e5613872ade6783
ikuki <akedaikuki@gmail.com>
2023-06-30 11:48:26 +0800

    todolist

---
+++ src/components/TodoCollection.jsx
@@ -0,0 +1,19 @@
+import TodoItem from './TodoItem';
+
+const TodoCollection = ({
+  todos,
+  onToggleDone,
+  onSave,
+  onDelete,
+  onChangeMode,
+}) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />;
+      })}
+    </div>
+  );
+};
+
+export default TodoCollection;
